0596eba4f917a5be7a71ce6927f035aa20c7a96b
jkuhl <mememaster720@protonmail.com>
2023-02-05 22:03:47 -0600

    - Improve wandering logic - Update colonist texture

--- src/Data/ECS/Components/Wander.cs
+++ src/Data/ECS/Components/Wander.cs
@@ -1,3 +1,4 @@
+using IslandGen.Data.Enum;
 using IslandGen.Services;
 
 namespace IslandGen.Data.ECS.Components;
@@ -5,7 +6,8 @@ namespace IslandGen.Data.ECS.Components;
 public class Wander : IComponent
 {
     private readonly int _delay;
-    private readonly (int, int) _init = (-1, -1);
+    private readonly int _distance;
+    
     private bool _delayActive;
     private int _delayCounter;
     private (int, int) _destination;
@@ -14,35 +16,59 @@ public class Wander : IComponent
     ///     Component that enables the entity to wander around the game map
     /// </summary>
     /// <param name="delay"> Amount of idle between destinations </param>
-    public Wander(int delay = 5)
+    /// <param name="distance"> Max distance the entity can wander at a time </param>
+    public Wander(int delay = 3, int distance = 5)
     {
         _delay = delay;
-        _delayActive = false;
+        _distance = distance;
+        
+        _delayActive = true;
         _delayCounter = 0;
-        _destination = _init;
     }
 
     public void Update(EntityBase entity)
     {
-        if (_destination == _init) _destination = ServiceManager.GetService<GameMap>().GetRandomTile();
-
         if (_delayActive)
         {
             _delayCounter++;
 
             if (_delayCounter >= _delay)
+            {
                 _delayActive = false;
+                _delayCounter = 0;
+                SetDestination(entity.GetMapPosition());
+            }
             else
+            {
                 return;
+            }
         }
 
         if (entity.GetMapPosition() == _destination)
         {
             _delayActive = true;
-            _destination = ServiceManager.GetService<GameMap>().GetRandomTile();
             return;
         }
 
         entity.MoveTo(_destination);
+    }
+
+    /// <summary>
+    ///     Randomly sets a new destination near the entity's current position
+    /// </summary>
+    /// <param name="currentMapPosition"> Current position on the game map </param>
+    private void SetDestination((int, int) currentMapPosition)
+    {
+        var gameMap = ServiceManager.GetService<GameMap>();
+        var rnd = ServiceManager.GetService<Random>();
+
+        (int, int) destination;
+        while (true)
+        {
+            destination = (currentMapPosition.Item1 + rnd.Next(-_distance, _distance), currentMapPosition.Item2 + rnd.Next(-_distance, _distance));
+            if (!gameMap.TileMap[destination.Item1, destination.Item2].IsWater()) break;
+        }
+
+        _destination = destination;
     }
 }
